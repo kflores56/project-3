8f9dcae9dd7fbf54b18018ce13a115275a508966
Kristin Flores <kristin@Joan-Steppenbeck.local>
2021-06-17 19:25:26 -0500

    update to app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, request
-from xgboost import XGBClassifier
+# from xgboost import XGBClassifier
 import pandas as pd
 import numpy as np
 import pickle
@@ -10,7 +10,7 @@ app = Flask(__name__)
 with open(f'xgb_model.pickle', "rb") as f:
     model = pickle.load(f)
 
-feature_names = model.get_booster().feature_names
+feature_names = model.feature_names
 
 # Route to render index.html template using data from Mongo
 @app.route("/", methods=["GET", "POST"])
@@ -18,18 +18,18 @@ def home():
     output_message = ""
 
     if request.method == "POST":
-        recency = float(request.form["recency"])
-        frequency = float(request.form["frequency"])
-        monetary = float(request.form["monetary"])
-        time = float(request.form["time"])
+        bedroom = float(request.form["bedroom"])
+        bathroom = float(request.form["bathroom"])
+        sqft = float(request.form["sqft"])
+        yearBuilt = float(request.form["yearBuilt"])
 
         # data must be converted to df with matching feature names before predict
-        data = pd.DataFrame(np.array([[recency, frequency, monetary, time]]), columns=feature_names)
+        data = pd.DataFrame(np.array([[bedroom, bathroom, sqft, yearBuilt]]), columns=feature_names)
         result = model.predict(data)
         if result == 1:
-            output_message = "Nice, you will donate soon, thank you ^_^"
+            output_message = "A home in Austin, TX will cost you XXX"
         else:
-            output_message = "Please consider donating :-("
+            output_message = "A home in Austin, TX will cost you XXX2"
     
     return render_template("index.html", message = output_message)
 

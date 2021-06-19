e404c277efd8060a2cc07ed2f2e179476c8abeaa
Gerald Watts <geraldwattsthethird@gmail.com>
2021-06-18 21:32:12 -0500

    app.py update

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
 from flask import Flask, render_template, redirect, request
-# from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from http.server import HTTPServer
 import pandas as pd
 import numpy as np
-import pickle
+# import pickle
+
+housing = pd.read_csv("austinHousingData.csv")
+
+X = housing[["yearBuilt", "livingAreaSqFt", "numOfBathrooms", "numOfBedrooms"]]
+y = housing["latestPrice"].values.reshape(-1, 1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+model = LinearRegression()
+model.fit(X, y)
 
 # Create an instance of Flask
 app = Flask(__name__)
 
-with open(f'xgb_model.pickle', "rb") as f:
-    model = pickle.load(f)
+# with open(f'xgb_model.pickle', "rb") as f:
+#     model = pickle.load(f)
 
-feature_names = model.feature_names
-print("----Feature----")
-print(feature_names)
-print("--------")
+# feature_names = model.predict(X)
+# print("----Feature----")
+# print(feature_names)
+# print("--------")
 
 # Route to render index.html template
 @app.route("/", methods=["GET", "POST"])
 def home():
-    output_message = ""
+    # output_message = ""
 
     if request.method == "POST":
         bedroom = float(request.form["bedroom"])
@@ -27,17 +39,17 @@ def home():
         yearBuilt = float(request.form["yearBuilt"])
 
         # data must be converted to df with matching feature names before predict
-        data = pd.DataFrame(np.array([[bedroom, bathroom, sqft, yearBuilt]]), columns=feature_names)
+        data = pd.DataFrame(np.array([[bedroom, bathroom, sqft, yearBuilt]]))
         print("---Data---")
         print(data)
         print("------")
         result = model.predict(data)
-        if result == 1:
-            output_message = "A home in Austin, TX will cost you XXX"
-        else:
-            output_message = "A home in Austin, TX will cost you XXX2"
+        # if result == 1:
+        #     output_message = "A home in Austin, TX will cost you"
+        # else:
+        #     output_message = "A home in Austin, TX will cost you XXX2"
     
-    return render_template("index.html", message = output_message)
+    return render_template("index.html", message = {result})
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
